4f19cfc353fbf80af1edae5eb4e2fdd2d0fa313d
Vignesh <120367609+vickymj007@users.noreply.github.com>
2023-06-30 21:52:12 +0530

    Delete node-day2-task directory

--- node-day2-task/Controllers/mongoData.js
+++
@@ -1,8 +0,0 @@
-// import { client } from "../db.js";
-
-// export function createNewRoom(data){
-//     return client
-//     .db('hall-booking-app')
-//     .collection('create-room')
-//     .updateOne(data)
-// }
--- node-day2-task/Routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const router = express.Router()
-
-router.get('/', (req,res)=>{
-    res.send("Hello there my Friend, This is a the Auth Page")
-})
-
-export default router;
