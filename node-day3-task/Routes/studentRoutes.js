abb50e2ee01ce994e239ef1738b8fd6b99158193
Vicky <vickypersish1997@gmail.com>
2023-07-07 16:05:44 +0530

    removed node_modules and env files

--- node-day3-task/Routes/studentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { createStudent, getStudents } from '../Controllers/studentController.js'
-
-const router = express.Router()
-
-router.get('/', getStudents)
-router.post('/', createStudent)
-
-export default router
